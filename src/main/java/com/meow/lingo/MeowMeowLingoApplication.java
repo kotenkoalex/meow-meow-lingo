4026afca57a35afb18aa8b376061eae116ccb61e
Alex <akotenko@prezna.com>
2024-01-22 11:38:03 -0500

    initial commit

---
+++ src/main/java/com/meow/lingo/MeowMeowLingoApplication.java
@@ -0,0 +1,13 @@
+package com.meow.lingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeowMeowLingoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeowMeowLingoApplication.class, args);
+    }
+
+}
